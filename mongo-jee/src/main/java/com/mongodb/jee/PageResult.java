f3d03211713a108327d1d73ec33a8d001a748d97
angelo.zerr@gmail.com <angelo.zerr@gmail.com>
2012-12-22 10:56:20 +0100

    Add cooments for page result.

--- mongo-jee/src/main/java/com/mongodb/jee/PageResult.java
+++ mongo-jee/src/main/java/com/mongodb/jee/PageResult.java
@@ -1,10 +1,29 @@
+/**
+ * Copyright (C) 2012 Angelo Zerr <angelo.zerr@gmail.com> and Pascal Leclercq <pascal.leclercq@gmail.com>
+ * 
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.mongodb.jee;
-
-import java.util.Iterator;
 
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 
+/**
+ * 
+ * Simple Pojo of page result.
+ * 
+ */
 public class PageResult {
 
 	private final Iterable<DBObject> items;
@@ -17,26 +36,46 @@ public class PageResult {
 				fromItemIndex, toItemIndex, cursor.count());
 	}
 
-	public PageResult(Iterable<DBObject> items, int fromItemIndex, int toItemIndex,
-			int totalItems) {
+	public PageResult(Iterable<DBObject> items, int fromItemIndex,
+			int toItemIndex, int totalItems) {
 		this.items = items;
 		this.fromItemIndex = fromItemIndex;
 		this.toItemIndex = toItemIndex;
 		this.totalItems = totalItems;
 	}
 
+	/**
+	 * Returns the paginated items.
+	 * 
+	 * @return
+	 */
 	public Iterable<DBObject> getItems() {
 		return items;
 	}
 
+	/**
+	 * Return the total items.
+	 * 
+	 * @return
+	 */
 	public int getTotalItems() {
 		return totalItems;
 	}
 
+	/**
+	 * Returns the from item index of the page.
+	 * 
+	 * @return
+	 */
 	public int getFromItemIndex() {
 		return fromItemIndex;
 	}
 
+	/**
+	 * Returns the to item index of the page.
+	 * 
+	 * @return
+	 */
 	public int getToItemIndex() {
 		return toItemIndex;
 	}
